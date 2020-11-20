40c978107f635e92c5870393021b4fdd53d99bc5
Georgianne Morra <gmorra@pr-k2.kzn.21-school.ru>
2020-11-20 16:58:05 +0300

    gnl

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,28 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gmorra <gmorra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 11:58:22 by gmorra            #+#    #+#             */
-/*   Updated: 2020/11/18 21:17:01 by gmorra           ###   ########.fr       */
+/*   Updated: 2020/11/20 16:51:54 by gmorra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static void			ft_strclr(char **s)
+{
+	if (*s)
+	{
+		free(*s);
+		*s = NULL;
+	}
+}
+
+static int			ft_return_neg(char *buf)
+{
+	free(buf);
+	buf = NULL;
+	return (-1);
+}
+
 static char			*check_remainer(char **remainer, char **line)
 {
 	char *ptr;
 
+	*line = NULL;
 	ptr = NULL;
 	if (*remainer)
 		if ((ptr = ft_strchr(*remainer, '\n')))
 		{
 			*ptr = '\0';
 			*line = ft_strdup(*remainer);
-			ft_strcpy(*remainer, ++ptr);
+			if (ptr == NULL)
+				ft_strclr(&(*remainer));
+			else
+				ft_strcpy(*remainer, ++ptr);
 		}
 		else
 		{
@@ -55,22 +75,23 @@ int					get_next_line(int fd, char **line)
 	static	char	*remainer[1024];
 	int				byte_read;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || !line || !(buf = malloc(BUFFER_SIZE + 1)))
-		return (-1);
-	*line = NULL;
+	buf = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line || !(buf = malloc(BUFFER_SIZE + 1))
+		|| (-1 == read(fd, buf, 0)))
+		return (ft_return_neg(buf));
 	ptr = check_remainer(&remainer[fd], line);
 	while (!ptr && (byte_read = read(fd, buf, BUFFER_SIZE)))
 	{
-		if (byte_read == -1)
-			return (-1);
 		buf[byte_read] = '\0';
 		if ((ptr = ft_strchr(buf, '\n')))
 		{
 			*ptr = '\0';
-			remainer[fd] = ft_strdup(++ptr);
+			if (!(remainer[fd] = ft_strdup(++ptr)))
+				return (ft_return_neg(buf));
 		}
 		tmp = *line;
-		*line = ft_strjoin(tmp, buf);
+		if (!(*line = ft_strjoin(tmp, buf)))
+			return (ft_return_neg(buf));
 	}
 	return (ft_return(byte_read, &remainer[fd], ptr, buf));
 }
